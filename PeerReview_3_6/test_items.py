0af5c28bf80710cb4b23da90f8a3e6028fab142b
d.akinfiev <d.akinfiev@oneliya.ru>
2023-11-10 17:46:07 +0300

    after review changes

--- PeerReview_3_6/test_items.py
+++ PeerReview_3_6/test_items.py
@@ -6,6 +6,6 @@ def test_check_add_to_cart_button_is_available(browser):
     link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(30)
-    add_to_cart_button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
-    assert add_to_cart_button.is_displayed(), "Add to cart button is not displayed"
+    add_to_cart_button = browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    assert len(add_to_cart_button) > 0, "Add to cart button is not displayed"
 
